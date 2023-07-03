4e85576a57da9512f05c2f9cc5f514a5c433384d
Eleonora <106757028+Eleonora-Y@users.noreply.github.com>
2023-07-03 13:36:13 +0300

    Update UserRepository.java

--- main-service/src/main/java/ru/practicum/ewm/user/repository/UserRepository.java
+++ main-service/src/main/java/ru/practicum/ewm/user/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package ru.practicum.ewm.user.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import ru.practicum.ewm.user.model.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
