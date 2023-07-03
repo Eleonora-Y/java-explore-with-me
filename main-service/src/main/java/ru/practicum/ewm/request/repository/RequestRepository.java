f8af9de9dfd4620597bf3c258dd33054eb03887a
Eleonora <106757028+Eleonora-Y@users.noreply.github.com>
2023-07-03 13:35:22 +0300

    Update RequestRepository.java

--- main-service/src/main/java/ru/practicum/ewm/request/repository/RequestRepository.java
+++ main-service/src/main/java/ru/practicum/ewm/request/repository/RequestRepository.java
@@ -1,6 +1,7 @@
 package ru.practicum.ewm.request.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import ru.practicum.ewm.request.model.Request;
 import ru.practicum.ewm.request.model.RequestStatus;
 
