9a1cce5b80a4b3f65ab32406b9b8d9803505615f
Eleonora <106757028+Eleonora-Y@users.noreply.github.com>
2023-07-03 13:47:47 +0300

    Update EventRepository.java

--- main-service/src/main/java/ru/practicum/ewm/event/repository/EventRepository.java
+++ main-service/src/main/java/ru/practicum/ewm/event/repository/EventRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.repository.Repository;
 import ru.practicum.ewm.category.model.Category;
 import ru.practicum.ewm.event.model.Event;
 import ru.practicum.ewm.event.model.enums.EventState;
