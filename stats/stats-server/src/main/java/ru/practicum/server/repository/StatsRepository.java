a31082825f4bb3438e4c814a917eeaa10ac95457
Eleonora <106757028+Eleonora-Y@users.noreply.github.com>
2023-07-04 23:57:41 +0300

    Update

--- stats/stats-server/src/main/java/ru/practicum/server/repository/StatsRepository.java
+++ stats/stats-server/src/main/java/ru/practicum/server/repository/StatsRepository.java
@@ -3,12 +3,14 @@ package ru.practicum.server.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 import ru.practicum.server.model.EndpointHit;
 import ru.practicum.server.model.ViewStats;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
+@Repository
 public interface StatsRepository extends JpaRepository<EndpointHit, Long> {
     @Query("SELECT new ru.practicum.server.model.ViewStats(eh.app, eh.uri, count (eh.ip)) " +
             "FROM EndpointHit AS eh " +
